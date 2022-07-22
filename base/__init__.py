774bf870cc29aeea38890ceb070e3a739889736f
Bassell Iddisah <bbasssell16@gmail.com>
2022-07-22 21:26:59 +0000

    Adding users route

--- base/__init__.py
+++ base/__init__.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail
+from flask_migrate import Migrate
+from flask_loginmanager import LoginManager
 from .config import Config
 
 db = SQLAlchemy()
+mail = Mail()
+# migrate = Migrate()
+loginmanager = LoginManager()
+loginmanager.login_view = "user.signin"
+loginmanager.login_message_category = "info"
 
 def create_app(config=Config):
 	app = Flask(__name__)
 	db.init_app(app)
-	from businesses.routes import bus
+	mail.init_app(app)
+	# migrate.init_app(app)
+	loginmanager.init_app(app)
+	from base.businesses.routes import bus
 	app.register_blueprint(bus)
 	return app
